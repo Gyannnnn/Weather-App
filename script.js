0951dc23bb2d351bda0c65a849a1ac8212a76708
Gyanranjan Patra <hi.gyanaranjanpatra@gmail.com>
2024-03-04 21:11:10 +0530

    updated and added enter button functionality

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ console.log("Hello From GYanranjan PAtra !!")
 const apikey = "e8dc68a709733f5550970bee768aaf8b";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
-const searchBtn  = document.getElementById("searchButton");
+const searchBtn = document.getElementById("searchButton");
 const weatherIcon = document.getElementById("weatherIcon");
 
 
 
 
-async function checkWeather(city){
+async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apikey}`);
     var data = await response.json();
     console.log(data)
@@ -18,20 +18,27 @@ async function checkWeather(city){
     document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
     document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
 
-    if(data.weather[0].main == "Clear"){
+    if (data.weather[0].main == "Clear") {
         weatherIcon.src = "clear.png";
-    }else if(data.weather[0].main == "Clouds"){
+    } else if (data.weather[0].main == "Clouds") {
         weatherIcon.src = "mk.png";
-    }else if(data.weather[0].main == "Rain"){
+    } else if (data.weather[0].main == "Rain") {
         weatherIcon.src = "rain.png";
-    }else if(data.weather[0].main == "Drizzle"){
+    } else if (data.weather[0].main == "Drizzle") {
         weatherIcon.src = "drizzle.png";
-    }else if(data.weather[0].main == "Mist"){
+    } else if (data.weather[0].main == "Mist") {
         weatherIcon.src = "mist.png";
     }
 
 }
-searchBtn.addEventListener("click",()=>{
+searchBox.addEventListener("keydown", function (event) {
+   
+    if (event.keyCode === 13) {
+
+        checkWeather(searchBox.value);
+    }
+});
+searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
 })
 
